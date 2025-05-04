2c2406518f848106f183cd3bb1485690c38b7991
vaibhav <vaibhavthakkar26051999@gmail.com>
2025-05-05 01:22:13 +0530

    client side api

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-import WhyChooseUsPage from "./component/WhyChooseUsPage";
+"use client";
+import { useEffect, useState } from "react";
+import WhyChooseUsPage, { WhyChooseUsProps } from "./component/WhyChooseUsPage";
 import { getTitleWithFeatures } from "./services/strapiService";
 
-export default async function Home() {
-  const data = await getTitleWithFeatures();
+export default function Home() {
+  const [fetchData, setFetchData] = useState<WhyChooseUsProps[]>([]);
+
+  // const data = await getTitleWithFeatures();
+
+  useEffect(() => {
+    getDataHandler();
+  }, []);
+
+  const getDataHandler = async () => {
+    try {
+      const data = await getTitleWithFeatures();
+      if(data){
+        setFetchData(data);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <main className="min-h-screen">
-  
-    <WhyChooseUsPage data={data ? data : []} />
+      <WhyChooseUsPage data={fetchData ? fetchData : []} />
     </main>
   );
 }
