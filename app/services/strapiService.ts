e9dcd1dc47f2611c03363b7b8afbad62ce9df8b6
vaibhav <vaibhavthakkar26051999@gmail.com>
2025-05-04 21:37:04 +0530

    Update strapiService.ts

--- app/services/strapiService.ts
+++ app/services/strapiService.ts
@@ -2,7 +2,6 @@
 import axios from "axios";
 import { WhyChooseUsProps } from "../component/WhyChooseUsPage";
 
-const API_URL = "http://192.168.1.8:1337/api";
 
 interface FeatureData {
   id: number;
@@ -32,9 +31,6 @@ export const getTitleWithFeatures =
       const response = await axios.get(
         "http://127.0.0.1:1337/api/why-choose-uses?populate[Features][populate]=*"
       );
-      // const response = await axios.get<StrapiResponse<TitleWithFeaturesData>>(
-      //   `${API_URL}/why-choose-us`,
-      // );
       return response.data.data;
     } catch (error) {
       console.error("Error fetching title with features:", error);
